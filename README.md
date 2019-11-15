ea2dba63983ff8330e9391f2d3f33f1bf52b4090
huge0518 <1207194298@qq.com>
2019-11-15 19:30:09 +0800

    打包天气

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # webp_weather
 webpack打包天气应用
+
+
+```txt
+npm i 插件(包)名 --save, 其中 --save表示将该包名添加到package.json文件的dependencies的字段中， --save 的简写 -S
+```
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+let path = require("path")
+
+let miniCssExtractPlugin = require("mini-css-extract-plugin")
+let htmlWebpackPlugin = require("html-webpack-plugin")
+
+//实例化插件
+let mini_css_extract_plugin = new miniCssExtractPlugin({
+    filename:"[name]+min.css"
+})
+
+let html_webpack_plugin = new htmlWebpackPlugin({
+    //要打包的模板路径
+    template:"./index.html",
+    inject:"body",
+    minify:{
+        removeComments:true,
+        removeAttributeQuotes:true,
+        collapseWhitespace:true,
+    },
+    filename:"index.html"
+});
+module.exports={
+    mode:"development",
+
+    entry:{
+        index:"./js/index.js",
+        rem:"./js/rem.js"
+    },
+
+    output:{
+        path: path.resolve(__dirname + "/build"),
+        filename:"[name].min.js"
+    },
+
+    module:{
+        rules:[
+
+            //处理css
+            {
+                test:/\.css$/,
+                use:[
+                    {loader:"style-loader"},
+                    {loader:"css-loader"}
+                ]
+            },
+
+            //处理图片路径
+            {
+                test:/\.(png|gif|jpg|jpeg)$/,
+                use:[
+                    {loader:"url-loader",
+                    //图片小于等于1024B
+                    options:{
+                        limit:1024
+                    }
+                }
+                ]
+            },
+            
+            //处理html模板中的图片
+            {
+                test:/\.html?$/,
+                use:[
+                    {loader:"html-withimg-loader"}
+                ]
+            }
+        ]
+    },
+
+    //配置插件
+    plugins:[
+        mini_css_extract_plugin,
+        html_webpack_plugin
+    ],
+
+    //配置本地服务
+    devServer:{
+        host:"127.0.0.1",
+        port:8001
+    }
+}
